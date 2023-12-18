62065eb64b909960e83980ebc987b4e3f14aea23
Wendel de Andrade <shindagami@gmail.com>
2023-12-18 00:36:29 -0300

    Página de chat usando o "react-chat-engine-pretty"

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,24 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import PropTypes from 'prop-types';
+import { PrettyChatWindow } from "react-chat-engine-pretty"
+
+const ChatsPage = (props) => {
+    return (
+        <div style={{ height: '100vh' }}>
+            <PrettyChatWindow
+                projectId="6ac4dba8-56cc-4624-968b-ee14b6726cf1"
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%' }}
+            />
+        </div>
+    )
 }
 
-export default ChatsPage
+ChatsPage.propTypes = {
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        secret: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default ChatsPage;
