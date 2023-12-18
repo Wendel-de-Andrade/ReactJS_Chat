2f931e9b84c4ea66d93bad0dc04dcfa7873b46ca
Wendel de Andrade <shindagami@gmail.com>
2023-12-17 23:25:07 -0300

    Criar usuário com chave da ChatEngine

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "private-key": "abced5f5-37a4-4651-beaa-8bee480ddd65" } }
+        )
+        return res.status(r.status).json(r.data)
+    } catch (error) {
+        return res.status(error.response.status).json(error.response.data)
+    }
 });
 
 app.listen(3001);
